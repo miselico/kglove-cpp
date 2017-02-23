a40e9c78b6abbae083067d76116a8839e13f4c48
michaelcochez <michael.cochez@>
2017-02-23 22:49:49 +0100

    Added test for BCA compute order speed

--- src/RDF2Co_occurence.cpp
+++ src/RDF2Co_occurence.cpp
@@ -119,7 +119,74 @@ TPair<TVec<TStr>, THash<TStr, int>> computePredicateIDs(TPt<TNodeEdgeNet<TStr, T
 namespace {
 static TStr RDF_TYPE("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>");
 static TStr OWL_THING("<http://www.w3.org/2002/07/owl#Thing>");
+
+/**
+ * Attempts to determine the order in which most BCA computations can be reused.
+ * This is using the heuristic that
+ *
+ * 1. nodes with zero out degree (or one for which all outgoing edges points to nodes with precomputed BCAs) should always be computed first
+ * 2. if no such node exists, then the node with highest in degree is selected
+ *
+ */
+TVec<TInt> determineBCVcomputeOrder(TPt<TNodeEdgeNet<TStr, TStr> > baseGraph) {
+
+	TPt<TNEGraph> prunable = TNEGraph::New();
+	for (TNodeEdgeNet<TStr, TStr>::TNodeI NI = baseGraph->BegNI(); NI < baseGraph->EndNI(); NI++) {
+		prunable->AddNode(NI.GetId());
+	}
+	for (TNodeEdgeNet<TStr, TStr>::TEdgeI EI = baseGraph->BegEI(); EI < baseGraph->EndEI(); EI++) {
+		prunable->AddEdge(EI.GetSrcNId(), EI.GetDstNId());
+	}
+	baseGraph = NULL; //making sure we do not accidentally write to the original
+	TVec<TInt> result;
+
+	while (prunable->GetNodes() > 0) {
+
+		TVec<TInt> withZeroOutDegree;
+		for (TNEGraph::TNodeI iter = prunable->BegNI(); iter < prunable->EndNI(); iter++) {
+			if (iter.GetOutDeg() == 0) {
+				withZeroOutDegree.Add(iter.GetId());
+			}
+		}
+		if (withZeroOutDegree.Len() > 0) {
+			//found some
+			result.AddV(withZeroOutDegree);
+			//remove from graph
+			for (TVec<TInt>::TIter ID = withZeroOutDegree.BegI(); ID < withZeroOutDegree.EndI(); ID++) {
+				prunable->DelNode(ID->Val);
+			}
+			continue;
+		}
+		//none with zero out degree. Take the one with highest indegree.
+		int highestIndegree = -1;
+		int withHighestInDegree = -1;
+		for (TNEGraph::TNodeI iter = prunable->BegNI(); iter < prunable->EndNI(); iter++) {
+			if (iter.GetInDeg() > highestIndegree) {
+				highestIndegree = iter.GetInDeg();
+				withHighestInDegree = iter.GetId();
+			}
+		}
+		if (withHighestInDegree == -1) {
+			cerr << "error, none found with in degree";
+			exit(5);
+		}
+		result.Add(withHighestInDegree);
+		prunable->DelNode(withHighestInDegree);
+	}
+	return result;
 }
+
+void testBCAComputeOrderSpeed(TStr filename) {
+	TPair<TPt<TNodeEdgeNet<TStr, TStr> >, THash<TStr, int> > graphAndNodeIndex = n3parser::buildRDFGraphIgnoreLiterals(filename);
+	TPt<TNodeEdgeNet<TStr, TStr> > graph = graphAndNodeIndex.Val1;
+	cout << "computing BCV compute order" << endl;
+	TVec<TInt, int> order;
+	order = determineBCVcomputeOrder(graph);
+	cout << "end determining BCV compute order" << endl;
+}
+
+}
+
 /**
  *
  * Compute the BCA score for each pair in the graph under the given weighing strategy.
@@ -135,10 +202,12 @@ void computeFrequenciesIncludingEdges(TStr filename, GraphWeigher& weighingStrat
 
 	TPt<TNodeEdgeNet<TStr, WeightedPredicate> > weightedGraph;
 	TPair<TVec<TStr>, THash<TStr, int>> predLabelsAndIDs;
+	TVec<TInt> order;
 
 	{ //scoping to save on total memory
 		TPair<TPt<TNodeEdgeNet<TStr, TStr> >, THash<TStr, int> > graphAndNodeIndex = n3parser::buildRDFGraphIgnoreLiterals(filename);
 		TPt<TNodeEdgeNet<TStr, TStr> > graph = graphAndNodeIndex.Val1;
+		order = determineBCVcomputeOrder(graph);
 		predLabelsAndIDs = computePredicateIDs(graph);
 		weightedGraph = weighingStrategy.weigh(graph);
 	}
@@ -189,7 +258,7 @@ void computeFrequenciesIncludingEdges(TStr filename, GraphWeigher& weighingStrat
 		}
 	}
 
-	//still need to write all predicates to the vocab file
+//still need to write all predicates to the vocab file
 
 	TVec<TStr> predicateLabels = predLabelsAndIDs.Val1;
 	for (TVec<TStr>::TIter it = predicateLabels.BegI(); it < predicateLabels.EndI(); it++) {
@@ -230,7 +299,7 @@ void computeFrequenciesIncludingEdgesTheUltimate(TStr filename, GraphWeigher& we
 
 	THash<TStr, int> predGraphIDs = predLabelsAndIDs.Val2;
 
-	//int predicateIDGloveOffset = weightedGraph->GetNodes();
+//int predicateIDGloveOffset = weightedGraph->GetNodes();
 
 	for (int i = 0; i < weightedGraph->GetNodes(); ++i) {
 
@@ -260,7 +329,7 @@ void computeFrequenciesIncludingEdgesTheUltimate(TStr filename, GraphWeigher& we
 			cout << i << "/" << weightedGraph->GetNodes() << " = " << i / float(weightedGraph->GetNodes()) << endl;
 		}
 	}
-	//still need to write all predicates to the vocab file
+//still need to write all predicates to the vocab file
 
 	TVec<TStr> predicateLabels = predLabelsAndIDs.Val1;
 	for (TVec<TStr>::TIter it = predicateLabels.BegI(); it < predicateLabels.EndI(); it++) {
@@ -340,23 +409,26 @@ void performExperiments() {
 //TStr file = "sample-wikidata-terms-fragment.nt";
 //TStr file = "sample-wikidata-terms.nt";
 	TStr file = "../../datasets/aifb_fixed_complete.nt";
-	//	TStr file = "SmallTest4.nt";
+//	TStr file = "SmallTest4.nt";
 
 	FILE* glove_input_file_out = fopen("glove_input_file_out.bin", "w");
 	FILE* glove_vocab_file_out = fopen("glove_vocab_file_out", "w");
 
-	//InversePredicateFrequencyWeigher weigher = InversePredicateFrequencyWeigher();
 
-	UniformWeigher weigher;
+	testBCAComputeOrderSpeed(file);
 
-	//computeFrequenciesPushBCA(file, weigher, outfile);
+//InversePredicateFrequencyWeigher weigher = InversePredicateFrequencyWeigher();
 
-	bool normalize = true;
-	bool onlyEntities = false;
-	computeFrequenciesIncludingEdges(file, weigher, 0.80, 0.0039, glove_input_file_out, glove_vocab_file_out, normalize, onlyEntities);
-	//computeFrequencies(file, weigher, 0.50, 0.05, glove_input_file_out, glove_vocab_file_out);
+//	UniformWeigher weigher;
 
-	//computeFrequenciesIncludingEdgesTheUltimate(file, weigher, weigher, 0.70, 0.0039, glove_input_file_out, glove_vocab_file_out, normalize);
+//computeFrequenciesPushBCA(file, weigher, outfile);
+
+//	bool normalize = true;
+//	bool onlyEntities = false;
+//	computeFrequenciesIncludingEdges(file, weigher, 0.80, 0.0039, glove_input_file_out, glove_vocab_file_out, normalize, onlyEntities);
+//computeFrequencies(file, weigher, 0.50, 0.05, glove_input_file_out, glove_vocab_file_out);
+
+//computeFrequenciesIncludingEdgesTheUltimate(file, weigher, weigher, 0.70, 0.0039, glove_input_file_out, glove_vocab_file_out, normalize);
 
 	fclose(glove_input_file_out);
 	fclose(glove_vocab_file_out);
